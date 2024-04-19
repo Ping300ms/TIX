d73865913baa74559b67af01464a335ecf2c2bc2
Marianne <marianne.buenos@epita.fr>
2024-04-19 22:22:27 +0200

    fix(utils): regex_get amélioré et regex_check opérationnel

--- src/utils/regutils.c
+++ src/utils/regutils.c
@@ -1,5 +1,4 @@
 #include "regutils.h"
-
 #include <regex.h>
 #include <string.h>
 #include <stdio.h>
@@ -12,13 +11,13 @@ char regex_check(char* string, char* pattern)
     char msgbuf[100];
   
     reti = regcomp(&regex, pattern, 0);
-    if (reti)
+    if (reti == 1)
     {
         fprintf(stderr, "TIX: Could not compile tag delimiters\n");
         exit(EXIT_FAILURE);
     }
     reti = regexec(&regex, string, 0, NULL, 0);
-    if (!reti)
+    if (reti == 0)
     {
         regfree(&regex);
         return 1;
@@ -40,33 +39,51 @@ char regex_check(char* string, char* pattern)
 char* regex_get(char* string, char* pattern)
 {
   // FIXME
-  regex_t regex;
-  regmatch_t* match = NULL;
-  int reti;
+  regex_t *regex = (regex_t*)malloc(sizeof(regex_t));
+  regmatch_t *match;
+
   
-  reti = regexec(&regex, pattern, 1, match, 0);
-  if(reti)
+  if (regcomp(regex, pattern,REG_EXTENDED|REG_NEWLINE))
+  {
+      fprintf(stderr, "TIX: Could not compile tag delimiters\n");
+      exit(EXIT_FAILURE);
+  }
+  match = (regmatch_t*)malloc(sizeof(regmatch_t)*(regex->re_nsub+1));
+
+  
+  if(regexec(regex, string, regex->re_nsub+1, match, 0))
   {
     fprintf(stderr, "TIX: No match in header\n");
     exit(EXIT_FAILURE);
   }
-   
-  int i = 0;
-  if(match[i].rm_so == -1)
+
+  
+  if(match[0].rm_so == -1)
   {
     fprintf(stderr, "TIX: No");
   }
-  int start = match[i].rm_so;
-  int finish = match[i].rm_eo;
-  char* res = malloc(sizeof(char) * (finish - start + 1));
-  sprintf(res, "%.*s", (finish - start), string + start);
+  int start = match[0].rm_so;
+  int finish = match[0].rm_eo;
+  char* res = (char*)malloc(((finish-start) + 1));
+  sprintf(res, "%.*s", finish-start , string + start);
+  regfree(regex);
+
   return res;
 }
 
-/*
-int main()
+/*int main()
 {
-  char* a = "Bonsoir pariiiiiiiiiis !!!";
-  printf("%s\n", regex_get(a, "i*"));
-}
-  */
+  char* a = "aagezo4556rgiperugbneroifejgfnerbons9,0ooooiroebzerrzberzer";
+  //char * r = regex_get(a, "([0-9]+)");
+  //printf("%s", r);
+  a = "Bonsoir partiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii !!!";
+  char *r = regex_get(a, "parti+ !");
+  printf("%s", r);
+  //r = regex_get(a, "ti");
+  //printf("%s", r);
+
+  /*printf("%d\n", regex_check(a, "i*"));
+  printf("%d\n", regex_check(a, "y.*"));
+  printf("%d\n", regex_check(a, "x"));
+
+}*/
