ae8bf9df3c80613b332699a00045d308caf457f0
pierre.maunier <pierre.maunier@epita.fr>
2024-04-19 21:35:58 +0200

    fix: added project-wide support for config tags (REGEX ARE NOT DONE)

--- src/extractor/extractor.c
+++ src/extractor/extractor.c
@@ -1,45 +1,66 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "../scanner/chunk.h"
 #include "../utils/list.h"
+#include "../utils/regutils.h"
 #include "test.h"
 #include "expected.h"
 #include "actual.h"
 #include "config.h"
 
-struct Expected* get_expected(struct Chunk* chunk)
+struct Expected* get_expected(char* tag)
 {
-  // FIXME
-  (void)chunk;
-  return NULL;
+  // FIXME DO THE REGEX
+  struct Expected* expected = malloc(sizeof(struct Expected));
+  expected->expected_out = regex_get(tag, "");
+  expected->expected_err = regex_get(tag, "");
+  expected->expected_code = regex_get(tag, "");
+  expected->command = regex_get(tag, "");
+  return expected;
 }
 
-struct Actual* get_actual(struct Chunk* chunk)
+struct Actual* get_actual()
 {
-  // FIXME
-  (void)chunk;
-  return NULL;
+  struct Actual* actual = malloc(sizeof(struct Actual));
+  actual->actual_out = NULL;
+  actual->actual_err = NULL;
+  actual->actual_code = NULL;
+  return actual;
 }
 
-struct Config* get_config(struct Chunk* chunk)
+struct Config* get_config(struct List* chunk_list)
 {
-  // FIXME
-  (void)chunk;
-  return NULL;
+  // FIXME DO THE REGEX
+  char* tag = NULL;
+  for (size_t i = 0 ; i < chunk_list->size; i++)
+  {
+    if (((struct Chunk*)chunk_list->elements[i])->is_config)
+      tag = chunk_list->elements[i];
+  }
+  if (tag == NULL)
+    fprintf(stderr, "TIX: No config tag found");
+  
+  struct Config* config = malloc(sizeof(struct Config));
+  config->executable = regex_get(tag, "");
+  config->file_extension = regex_get(tag, "");
+  config->exec_path = regex_get(tag, "");
+  return config;
 }
 
 struct List* extract(struct List* chunk_list)
 {
+  struct Config* config = get_config(chunk_list);
   struct List* test_list = list_create();
   for (size_t i = 0; i < chunk_list->size; i++)
   {
     struct Chunk* chunk = chunk_list->elements[i];
-
-    struct Expected* expected = get_expected(chunk);
-    struct Actual* actual = get_actual(chunk);
-    struct Config* config = get_config(chunk);
-
-    list_append(test_list, test_create(expected, actual, config));
+    for (size_t j = 0; j < chunk->tags->size; j++)
+    {
+      struct Expected* expected = get_expected(chunk->tags->elements[j]);
+      struct Actual* actual = get_actual();
+      list_append(test_list, test_create(expected, actual, config));
+    }
   }
 
   return test_list;
--- src/extractor/extractor.h
+++ src/extractor/extractor.h
@@ -1,4 +1,13 @@
 #ifndef EXTRACTOR_H
 #define EXTRACTOR_H
 
+#include "config.h"
+#include "expected.h"
+#include "actual.h"
+
+struct Expected* get_expected(char* tag);
+struct Actual* get_actual();
+struct Config* get_config(struct List* chunk_list);
+struct List* extract(struct List* chunk_list);
+
 #endif
